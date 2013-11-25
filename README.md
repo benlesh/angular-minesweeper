8bbaa653dab3bc8c4a30369b3cc01899298f1c9d
Ben Lesh <ben@benlesh.com>
2013-11-25 14:13:48 -0500

    Add grid creation

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ A step by step guide to creating a very simple minesweeper game in Angular.
 
 To view the various "steps" in development, (or at least the development in different stages as it progresses), please
 checkout the various branches.
+
+----
+
+# Step 1: Let's create the game board.
+
+1. Add the `web/index.html`. Make sure that you're referencing `../bower_components/angular/angular.js` and
+`js/minsweeper.js` (which is the location of our final .js file we're concatenating with our Grunt process)
+2. Add a controller file: `src/Controllers/MineSweeperCtrl.js' this will be the primary controller for our Minesweeper
+game.
+3. Add the module reference to bootstrap our app to your html tag: `<html ng-app="minesweeper">`.
+4. Add a `<div>` tag to the body that references our `MineSweeperCtrl`: `<div ng-controller="MineSweeperCtrl"></div>`.
+5. Add some code to generate the grid for our board to the controller `MineSweeperCtrl.js`.
+6. Add some repeaters to the html to display our grid.
--- web/js/minesweeper.js
+++ web/js/minesweeper.js
@@ -1,1 +1,33 @@
 angular.module('minesweeper', []);
+angular.module('minesweeper').controller('MineSweeperCtrl',
+    [
+        '$scope',
+        function ($scope) {
+            var ctrl = this;
+
+            /**
+             * returns an array of arrays with the outer array containing rows, and the inner array
+             * containing columns within those rows.
+             * @param width
+             * @param height
+             * @returns {Array}
+             */
+            ctrl.createGrid = function (width, height) {
+                var grid = [],
+                    row, x, y;
+                for (y = 0; y < height; y++) {
+                    row = [];
+                    for (x = 0; x < width; x++) {
+                        row.push(null);
+                    }
+                    grid.push(row);
+                }
+                return grid;
+            };
+
+            $scope.gridWidth = 20;
+            $scope.gridHeight = 20;
+
+            $scope.grid = ctrl.createGrid($scope.gridWidth, $scope.gridHeight);
+        }
+    ]);
