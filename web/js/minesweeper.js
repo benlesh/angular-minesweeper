604eb0acddb2a9b3191e62f8e59f7f1cfd1033b2
Ben Lesh <ben@benlesh.com>
2013-12-10 10:39:50 -0500

    add rightClick directive

--- web/js/minesweeper.js
+++ web/js/minesweeper.js
@@ -153,3 +153,18 @@ angular.module('minesweeper').controller('MineSweeperCtrl',
             };
         }
     ]);
+
+angular.module('minesweeper').directive('rightClick', function($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+            var cmd = $parse(attrs.rightClick);
+            elem.on('contextmenu', function(e) {
+                scope.$apply(function (){
+                    cmd(scope);
+                    e.preventDefault();
+                });
+            });
+        }
+    };
+});
