4c78120661fe5d85f11506bf6fd67ae26c7ce2d8
Ben Lesh <ben@benlesh.com>
2014-01-14 14:21:42 -0500

    add onconnect

--- server/MinesweeperServer/MinesweeperServer/MinesweeperHub.cs
+++ server/MinesweeperServer/MinesweeperServer/MinesweeperHub.cs
@@ -8,9 +8,27 @@ namespace MinesweeperServer
 {
     public class MinesweeperHub : Hub
     {
+        public readonly Dictionary<string, MinesweeperUser> UserList = new Dictionary<string, MinesweeperUser>(); 
+
         public void Ping()
         {
             Clients.All.onServerMessage("Pong!");
         }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            UserList.Add(Context.ConnectionId, new MinesweeperUser
+                {
+                    ConnectionId = Context.ConnectionId,
+                    Name = "Anonymous"
+                });
+            return base.OnConnected();
+        }
+    }
+
+    public class MinesweeperUser
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
     }
 }
